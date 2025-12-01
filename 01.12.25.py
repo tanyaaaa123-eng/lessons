1784d1aec4050ef36c255317bad0f27f4b2eb753
azimut78 <tanya.mitina.09@mail.ru>
2025-12-01 17:31:23 +0200

    new

--- 01.12.25.py
+++ 01.12.25.py
@@ -6,3 +6,39 @@
 # control y - вернуть изменения
 
 print("Hello world")
+name = "hello"
+a=0
+b=0
+# возвращает память где переменная  хранится значение
+a=b
+print(id(a),id(b))
+# int - целочисленный
+# a=10
+# print(type(a))
+# str - символьный тип данys[
+# a=""
+# bool - логический тип данных
+# a=True
+# print(type(a))
+# list - список элементов
+# a=[1,"hello", True]
+# print(type(a))
+# float -  число с плавающей точкой
+# a=1.5
+# print(a,type(a))
+# a=.5
+# print(a,type(a))
+# a = 5.
+# print(a, type(a))
+# dict - {key:value} - набор элементов ключ значение
+# d ={"key":"value"," key2":"value2"}
+# print(d["key"])
+# set - набор уникальных элементов
+a = {1,2,3,4,5,5,5,5}
+print(a,type(a))
+# tuple - кортеж (набор неизменяемых значений)
+a = (1,2,3,4,5,6)
+b = [1,2,3,4,5]
+b[2] = 10
+# a[2] = 10
+print(b)
