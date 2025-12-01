97f6f3e5d54298b0bef52d636de6dad72d9b0db4
azimut78 <tanya.mitina.09@mail.ru>
2025-12-01 16:35:49 +0200

    посмотрели на комментарий

--- 01.12.25.py
+++ 01.12.25.py
@@ -1,1 +1,8 @@
+# contrl c - копирование
+# control v - вставить
+# contrl / - комментарий
+# control v - вставить
+# control z - откатить изменения
+# control y - вернуть изменения
+
 print("Hello world")
