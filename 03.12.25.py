a376c5f2ec450bffb62e2183256b9447151adec8
azimut78 <tanya.mitina.09@mail.ru>
2025-12-04 17:36:20 +0200

    new file

---
+++ 03.12.25.py
@@ -0,0 +1,62 @@
+# x=10
+# print(help(x))
+# sep- разделитель элементов по умолчанию
+# end - чем оканчивается последний элемент \n
+# \n - перевод на новую строку
+# \t - отступ в 4 пробела
+# \ - обозначение для скрытия спецсимвола(исключая команды)
+# print("hello","world", sep="", end ="")
+# print("hello2","world2\n\n\n\n")
+# print("hello2","world2")
+
+# a = int(float(input("введите символ:")))
+# # 10.13 - строка
+# # 10.13- float
+# #10 - int
+# print(a+1)
+# complex>float>int
+
+#  a + bj
+# a- действительня часть
+# b -  действительная
+# j - мнимая 1
+#  константа - постояная
+# z1= 3 + 4j
+# z2= 1 - 2j
+# print( z1 + z2)
+# print(z1.real, z1.imag)
+
+# print("hello" + "world")
+# print("hello" * 10)
+# a = int(float(input()))
+# b = int(float(input()))
+# print(a+b)
+# a = input()
+# b = input()
+# c = input()
+# print(a,b,c,sep = ":")
+#
+# numbers= [1,2,3,2,1]
+# d = tuple(set(numbers))
+# print(d)
+# a = input()
+# b = input()
+# c = input()
+# print(a,b,c, sep="-", end=".")
+#
+# a = int(input())
+# b = float(input())
+# c = str(input())
+# print(a,b,c, sep=",")
+#
+# text= "Python"
+# a= list(text)
+# print(a)
+#
+# s = "Python"
+# print(len(s))
+# help - документация по переданому объекту в консоли
+print(help(len))
+# x.__sizeof__()) - размер в битах переменой x
+
+
