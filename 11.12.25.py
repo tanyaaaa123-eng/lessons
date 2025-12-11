9cb0862a748cb86716018470e57ade3fd0788076
azimut78 <tanya.mitina.09@mail.ru>
2025-12-11 17:26:25 +0200

    new file

---
+++ 11.12.25.py
@@ -0,0 +1,93 @@
+# == > < != <= >=
+# == - равенство a == b ( a = b )
+# a = 5
+# b = 5
+# print(a == b)
+# #  bool
+# print( a == 5.0)
+# print ( 0 == False)
+# a = (1,2,3) # tuple
+# b = [1,2,3] # list
+# print ( a == b) # false
+# a = 0.1 + 0.2
+# print (round (a,1) == 0.3)
+# print (1 != 1) # false
+# print (1 != 2) # true
+# print(3 < 5 ) # true
+# print(1 + 0j < 2 + 0j) # комплексные не поддерживаются
+# a = "a"
+# b = ("b")
+# print ( a < b)
+# a = 5
+# print(1 < a < 10)
+# print( 1 <= 2)
+# is
+#  ==
+# a = 4
+# b = 4
+# print(a == b)
+# print ( a is b)
+# print( id(a) , id(b))
+# in/not in
+# numbers = [1,2,2,3,4]
+# print( 1 in numbers)
+# print( 1 not in numbers)
+# text = "python"
+# print ("py" in text)
+import math
+
+person = { "first name": "Join", "last name":"Smith"}
+s = [1,2,3]
+print( 0 in s)
+print(person["first name"])
+
+# not and or
+#  <= ^ ==
+# a = True
+# b = True
+# print(not a)
+# print(True == False )
+# # ->
+# # импликация
+# print(True <= False)
+# исключающее или
+# истинно только тогда когда одно из значений фолс а другое тру
+# print ( a ^ b)
+#  1 логические операторы
+#  cкобки > инверсия > конъюнкция(and) > or >
+# импликации > ==
+# 2 python
+# () > * , /,//,% > + , - > щперации сравнения > логические операторы
+# a = "123"
+# print(int(a))
+# a = "3f"
+# print(int(a,27))  #int перевод в 10
+# print(bin(21)) # bin в двоичную
+# print(oct(21)) # в восьмеричную
+# print(hex(21)) #  в шестнад
+# print(hex(20)[2:])
+# #  o - для восьмеричной
+# # b - для двоичной
+# # x -  для 16- ричной
+# print(format(20, 'x'))
+# print(f'{20:x}')
+# import math
+# print(math.pi)
+# from math import pi
+# print(pi)
+from math import *
+# в какую степень нужно возвести основание чтобы получить значение в скобках
+# print(math.sqrt(16))
+# print(math.ceil(1.3)) # в бльшую сторону округляет
+# print(math.floor(1.3)) #  в меньшую сторону
+# print(math.log10(100))
+# print(math.log2(8))
+# print(math.log(81,3))
+# pi, e
+# radians(100)
+# из радиан в градусы
+# degrees()
+# a = sin(180)
+# print(a)
+
+
