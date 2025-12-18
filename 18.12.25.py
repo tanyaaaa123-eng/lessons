0d9ea4b47e0a60a5dad8454e9047308335b4475c
azimut78 <tanya.mitina.09@mail.ru>
2025-12-18 17:36:05 +0200

    new file

---
+++ 18.12.25.py
@@ -0,0 +1,85 @@
+# from math import *
+# # i_2 = log2(2 ** 28)
+# # i = log2(2 ** 30)
+# # 2 ** 28 # глубина цвета
+# L= 7
+# N = 36
+# other = 9
+# users = 30
+# i = ceil(log2(N)) # 6
+# I = ceil( L * i / 8)
+# I_30 = (I + other) * users
+
+# False/True == <= and or
+#"", set() [] - False  пустные значения возврашают ноль
+# x = int(input("введите число"))
+# print(x)
+# if x > 0:
+#     print("положительное")
+#
+# elif x == "9" :
+#     print("9")
+# else:
+#     print("отрицательное")
+# n = int(input())
+# if n > 0 :
+#     print("+")
+# else:
+#     print("-")
+# n = int (input())
+# if n % 2 == 0:
+#     print("четное")
+# else:
+#     print("нечетное")
+# n = int(input())
+# if n > 0 :
+#     if n % 2 == 0 and n % 10 != 0:
+#         print("")
+#     elif n % 2 == 1:
+#         print("")
+#     elif n % 10 == 0 :
+#         print("")
+# else:
+#     print("")
+# n = int(input())
+# b = int(input())
+# if n > b:
+#     if n % 10 == 0:
+#         print("больше и окан на 0")
+#     elif n % 2 == 1:
+#         print("больше и нечетное")
+#     else n:
+#         print("больше и окан на 0")
+# else:
+#     print("второе больше  или равно")
+#
+# n = int(input())
+# b = int(input())
+# if n >0 and b > 0 :
+#     print(n + b)
+# elif n < 0 or b <0:
+#     print(n * b)
+# else:
+#     print("оба ноль")
+# a = int(input())
+# if (a % 4 == 0 and a % 100 != 0) or a % 400 == 0:
+#     print("високосный")
+# else:
+#     print("не високосный")
+# находит модуль от числа
+# a = abs(-10)
+x1 = int(input())
+y1= int(input())
+x2 = int(input())
+y2= int(input())
+if abs(x1-x2)==2 and abs(y1-y2)==1 or abs(x1-x2)==1and abs(y1-y2)==2:
+    print("")
+else:
+    print("")
+
+
+
+
+
+
+
