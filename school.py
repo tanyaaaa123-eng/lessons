46b5aadfb2d6aedddcc48b1c54ff54cf799c0ced
tanya <tanya.mitina.09@mail.ru>
2026-03-23 15:38:17 +0200

    дз

---
+++ school.py
@@ -0,0 +1,42 @@
+# 1
+# from itertools import product, repeat
+# p = product("дгиаше",repeat = 5)
+# k = 1
+# for x in p:
+#     s = "".join(x)
+#     if s[0] != "а" and s[0] != "и" and s[0] != "э" and s[4] != "г" and s[4] != "д" and s[4] != "ш":
+#         print(k,s)
+#         k += 1
+#         print(k,s)
+# # 2
+# from itertools import product
+# p = product("абдеоп",repeat = 6)
+# k = 1
+# for x in p:
+#     s = "".join(x)
+#     if s[0]=="о" and s.count("a")==1 and s.count("б")==1 and s.count("д")==1 and s.count("е")==1 and s.count("о")==1 and s.count("п")==1:
+#         print(k,s)
+#     k +=1
+# #4
+# from itertools import product
+# p = product("масло",repeat = 6)
+# for x in p:
+#     s = "".join(x)
+#     if s.count("с")==1 and s[0]!="а" and s[5]!="м" and s[5]!="л" and s[5]!="с" and s[0]!="о":
+#         k +=1
+#         print(k,s)
+# for A in range(0,100):
+#     if all(((2*x+y!=70)or(x<y)or(A<x)) for x in range(0,100) for y in range(0,100)):
+#         print(A)
+#файлы
+with open("17-1.txt",mode="r",encoding="utf-8") as f:
+    l = list(map(int,f))
+    c = []
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            a1 = l[i]
+            a2 = l[j]
+            if (a1+a2)%17==0:
+                c.append(a1+a2)
+print(len(c),max(c))
+
